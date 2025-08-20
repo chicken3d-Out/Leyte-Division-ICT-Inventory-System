1aec119a6b4cecd148c5674c8cb2122dd5bd6444
chicken3d_Out <vincent.ontuca@evsu.edu.ph>
2025-08-20 09:51:34 +0800

    Added charts

--- src/app/pages/statistics/statistics.ts
+++ src/app/pages/statistics/statistics.ts
@@ -2,14 +2,46 @@ import { Component } from '@angular/core';
 import {ChangeDetectionStrategy} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartData, ChartOptions } from 'chart.js';
+import { Chart } from 'chart.js';
+
+// Register everything from Chart.js
+import { registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-statistics',
-  imports: [MatButtonModule, MatCardModule],
+  imports: [MatButtonModule, MatCardModule, BaseChartDirective],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './statistics.html',
   styleUrl: './statistics.css'
 })
 export class Statistics {
+
+  //Chart 1
+    chartData1: ChartData<'bar'> = {
+      labels: ['Laptop', 'Desktop', 'TV', 'Printer'],
+      datasets: [
+        { label: 'Equipments', data: [65, 59, 80, 81], backgroundColor: 'darkblue' },
+      ],
+    };
+
+    chartOptions1: ChartOptions = {
+      responsive: true,
+    };
+
+    //Chart 2
+    chartData2: ChartData<'doughnut'> = {
+      labels: ['Functional', 'Non-Functional', 'For Condemn'],
+      datasets: [
+        { label: 'Status', data: [120, 150, 90], backgroundColor: ['#FF6384','#36A2EB','#FFCE56'] },
+        
+      ],
+    };
+
+    chartOptions2: ChartOptions = {
+      responsive: true,
+    };
 
 }
